00d201e98fda486fb0edc79405648c11591cf493
Johannes <150372753+johannesschiessl@users.noreply.github.com>
2025-12-26 19:16:37 +0100

    better example

--- example/convex/storage.ts
+++ example/convex/storage.ts
@@ -9,16 +9,17 @@ export const routes = {
   imagePost: route({
     fileTypes: ["image/*"],
     maxFileSize: 1024 * 1024 * 5, // 5MB
-    onUploaded: async (ctx, { request, storageIdsAndUrls }) => {
+    checkUpload: async (ctx, request) => {
+      console.log("Request:", request);
+      return { test: "test" as const };
+    },
+    onUploaded: async (ctx, { request, storageIdsAndUrls, metadata }) => {
       console.log("Request:", request);
       console.log("Files:", storageIdsAndUrls);
-      return {
-        success: true,
-        message: "Images uploaded successfully",
-      };
+      return { success: true, test: metadata.test };
     },
   }),
-}
+};
 
 export const storageComponent = createClient(components.storage, { routes });
 
